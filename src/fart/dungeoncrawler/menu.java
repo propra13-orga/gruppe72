d5d39197c0e6c1f1806a7734fdadb7a1ef02c1d7
erhan abdurahman <erhan.abdurahman@uni-duesseldorf.de>
2013-05-21 17:42:10 +0200

    erhans menu

---
+++ src/fart/dungeoncrawler/menu.java
@@ -0,0 +1,38 @@
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class menu {
+
+	public static void main(String[] args){
+		JFrame frame = new JFrame("Menu");
+		frame.setVisible(true);
+		frame.setSize(600,400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar menubar = new JMenuBar();
+		frame.setJMenuBar(menubar);
+		
+		JMenu file = new JMenu("Spiel");
+		menubar.add(file);
+		JMenuItem exit = new JMenuItem("Beenden");
+		file.add(exit);
+		
+		JMenu help = new JMenu("Hilfe");
+		menubar.add(help);
+		JMenuItem about = new JMenuItem("About[FAQ]");
+		help.add(about);
+		
+		class exitaction implements ActionListener{
+			public void actionPerformed (ActionEvent e){
+				System.exit(0);
+			}
+			
+		}
+		
+		exit.addActionListener(new exitaction());
+	}
+}
